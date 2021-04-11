5e8b11189994fe6b91b09dcb53fd8ed4c22283c4
CalmCatStudio <54416290+CalmCatStudio@users.noreply.github.com>
2021-04-10 21:41:05 -0400

    Update ShuffleExtensions.cs

--- ShuffleExtensions.cs
+++ ShuffleExtensions.cs
@@ -3,64 +3,67 @@ using System.Collections.Generic;
 
 namespace CalmCatExtensions
 {
-    /// <summary>
-    /// Adds an extension method to Shuffle groups of objects with the Fisher-Yates shuffle
-    /// </summary>
-    public static class ShuffleExtensions
+    namespace Shuffle
     {
         /// <summary>
-        /// Shuffles the contents of the list
+        /// Adds an extension method to Shuffle groups of objects with the Fisher-Yates shuffle
         /// </summary>
-        /// <param name="random">The Random instance to use for shuffling</param>
-        public static void Shuffle<T>(this List<T> list, Random random)
+        public static class ShuffleExtensions
         {
-            int count = list.Count;
-            for (int i = 0; i < count; i++)
+            /// <summary>
+            /// Shuffles the contents of the list
+            /// </summary>
+            /// <param name="random">The Random instance to use for shuffling</param>
+            public static void Shuffle<T>(this List<T> list, Random random)
             {
-                int randomNumber = random.Next(i, count);
+                int count = list.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    int randomNumber = random.Next(i, count);
 
-                var temp = list[i];
+                    var temp = list[i];
 
-                list[i] = list[randomNumber];
-                list[randomNumber] = temp;
+                    list[i] = list[randomNumber];
+                    list[randomNumber] = temp;
+                }
             }
-        }
 
-        /// <summary>
-        /// Creates a new instance of Random, and shuffles the contents of the list with it
-        /// </summary>
-        public static void Shuffle<T>(this List<T> list)
-        {
-            Random random = new Random();
-            list.Shuffle(random);
-        }
-
-        /// <summary>
-        /// Shuffles the contents of the array
-        /// </summary>
-        /// <param name="random">The instance of random to shuffle with</param>
-        public static void Shuffle<T>(this T[] array, Random random)
-        {
-            int count = array.Length - 1;
-            for (int i = 0; i < count; i++)
+            /// <summary>
+            /// Creates a new instance of Random, and shuffles the contents of the list with it
+            /// </summary>
+            public static void Shuffle<T>(this List<T> list)
             {
-                int randomNumber = random.Next(i, count);
-
-                var temp = array[i];
-
-                array[i] = array[randomNumber];
-                array[randomNumber] = temp;
-
+                Random random = new Random();
+                list.Shuffle(random);
             }
-        }
 
-        /// <summary>
-        /// Creates a new instance of Random and shuffles the contents of the array with it.
-        /// </summary>
-        public static void Shuffle<T>(this T[] array)
-        {
-            Random random = new Random();
-            array.Shuffle(random);
-        }
+            /// <summary>
+            /// Shuffles the contents of the array
+            /// </summary>
+            /// <param name="random">The instance of random to shuffle with</param>
+            public static void Shuffle<T>(this T[] array, Random random)
+            {
+                int count = array.Length - 1;
+                for (int i = 0; i < count; i++)
+                {
+                    int randomNumber = random.Next(i, count);
+
+                    var temp = array[i];
+
+                    array[i] = array[randomNumber];
+                    array[randomNumber] = temp;
+
+                }
+            }
+
+            /// <summary>
+            /// Creates a new instance of Random and shuffles the contents of the array with it.
+            /// </summary>
+            public static void Shuffle<T>(this T[] array)
+            {
+                Random random = new Random();
+                array.Shuffle(random);
+            }
+        }   
     }
 }
